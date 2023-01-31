df5acaa2216a761d84923b7347802c4a7c2f3041
gamerslink <100190888+gamerslink@users.noreply.github.com>
2023-01-31 13:46:38 +0800

    初始化 Spring Boot Project

---
+++ src/main/java/com/liaojacky/springbootmenu/SpringbootMenuApplication.java
@@ -0,0 +1,13 @@
+package com.liaojacky.springbootmenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMenuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMenuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liaojacky/springbootmenu/SpringbootMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liaojacky.springbootmenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMenuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
